86e19d349c1a4ce5bbeff217538ba024dc328088
oliverkiss96 <oliver96kiss@gmail.com>
2022-05-28 01:36:08 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,14 @@ if (confirm("Press a button!")) {
     txt = "You pressed OK!";
   } else {
     txt = "You pressed Cancel!";
-  }  if (confirm("Press a button!")) {
   }
   if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+
+
+
+
+  
